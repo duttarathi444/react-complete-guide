bf58d9b300c41552d7930887f68cdd62de7999f0
emudikhana <emudiwzg@emudikhana.com>
2020-10-06 20:08:47 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import person from './Person/Person';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    person: [
+      { name: 'Max', age: '28' },
+      { name: 'Manu', age: "29" },
+      { name: 'stephanie', age: '22' }
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      person: [
+        { name: 'Rathindranath Dutta', age: '22' },
+        { name: 'Manu', age: "29" },
+        { name: 'stephanie', age: '23' }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age} />
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>My hobbie Racing</Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age} />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-var person = function () {
-    return <p> I am a Person</p>;
+var person = function (props) {
+    return (
+        <div>
+            <p> I am a {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
+    );
 }
 
 export default person;
