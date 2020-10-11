54efc78070c1567fba01e62588c5901cc292dbb7
emudikhana <emudiwzg@emudikhana.com>
2020-10-12 03:34:07 +0530

    add

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Radium from 'radium';
 import '../Person/Person.css'
 
 var person = function (props) {
+
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
+
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick={props.click}> I am a {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name}></input>
@@ -11,4 +19,4 @@ var person = function (props) {
     );
 }
 
-export default person;
+export default Radium(person);
