3e0b746b12bc46e21d0ce7b55f285f490c2df0a3
emudikhana <emudiwzg@emudikhana.com>
2020-10-17 02:40:48 +0530

    add

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Radium, { StyleRoot } from 'radium';
 
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+// import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 // import person from './Person/Person';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
   state = {
@@ -74,13 +74,11 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}>
-              <Person click={() => { this.deletePersonHandler(index) }}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                change={(event) => { this.nameChangedHandler(event, person.id) }} />
-            </ErrorBoundary>
+            return <Person click={() => { this.deletePersonHandler(index) }}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              change={(event) => { this.nameChangedHandler(event, person.id) }} />
           })}
         </div>
       );
